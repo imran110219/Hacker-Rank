19a9fed12902d6faa820ff748f9caf221d9bb170
imran110219 <imran110219@gmail.com>
2020-01-18 23:37:52 +0600

    Code added

---
+++ Java/27.Java 1D Array.java
@@ -0,0 +1,20 @@
+
+public class Solution {
+
+    public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        scan.close();
+
+        int[] a = new int[n];
+        for(int i = 0 ; i < n; i++){
+            int val = scan.nextInt();
+            a[i] = val;
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
